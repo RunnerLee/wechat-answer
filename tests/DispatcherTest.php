534de6cbc1e40da20781c11e1559e09ef2b3d444
Runnerlee <runnerleer@gmail.com>
2017-04-28 18:29:25 +0800

    update readme

---
+++ tests/DispatcherTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 2017-04
+ */
+
+use Runner\WechatAnswer\Dispatcher;
+
+class DispatcherTest extends PHPUnit_Framework_TestCase
+{
+
+
+
+}
